f2f03119421924c8d456261ac7fab2c7cadc384c
莫伟荣 <ajdwfnhaps@qq.com>
2019-03-04 22:34:16 +0800

    add OAuth Package

---
+++ src/AAM.Util.OAuth/Weixin/WeixinAuthenticationOptions.cs
@@ -0,0 +1,53 @@
+﻿/*
+ * Licensed under the Apache License, Version 2.0 (http://www.apache.org/licenses/LICENSE-2.0)
+ * See https://github.com/aspnet-contrib/AWA.Util.OAuth.Providers
+ * for more information concerning the license and the contributors participating to this project.
+ */
+
+using System.Security.Claims;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.OAuth;
+using Microsoft.AspNetCore.Http;
+using Newtonsoft.Json.Linq;
+using static AWA.Util.OAuth.Weixin.WeixinAuthenticationConstants;
+
+namespace AWA.Util.OAuth.Weixin
+{
+    /// <summary>
+    /// Defines a set of options used by <see cref="WeixinAuthenticationHandler"/>.
+    /// </summary>
+    public class WeixinAuthenticationOptions : OAuthOptions
+    {
+        public WeixinAuthenticationOptions()
+        {
+            ClaimsIssuer = WeixinAuthenticationDefaults.Issuer;
+            CallbackPath = new PathString(WeixinAuthenticationDefaults.CallbackPath);
+
+            AuthorizationEndpoint = WeixinAuthenticationDefaults.AuthorizationEndpoint;
+            TokenEndpoint = WeixinAuthenticationDefaults.TokenEndpoint;
+            UserInformationEndpoint = WeixinAuthenticationDefaults.UserInformationEndpoint;
+
+            Scope.Add("snsapi_login");
+            Scope.Add("snsapi_userinfo");
+
+            ClaimActions.MapJsonKey(ClaimTypes.NameIdentifier, "unionid");
+            ClaimActions.MapJsonKey(ClaimTypes.Name, "nickname");
+            ClaimActions.MapJsonKey(ClaimTypes.Gender, "sex");
+            ClaimActions.MapJsonKey(ClaimTypes.Country, "country");
+            ClaimActions.MapJsonKey(Claims.OpenId, "openid");
+            ClaimActions.MapJsonKey(Claims.Province, "province");
+            ClaimActions.MapJsonKey(Claims.City, "city");
+            ClaimActions.MapJsonKey(Claims.HeadImgUrl, "headimgurl");
+            ClaimActions.MapCustomJson(Claims.Privilege, user =>
+            {
+                var value = user.Value<JArray>("privilege");
+                if (value == null)
+                {
+                    return null;
+                }
+
+                return string.Join(",", value.ToObject<string[]>());
+            });
+        }
+    }
+}
